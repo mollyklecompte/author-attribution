ae860aa3f66f9b0ebc7b60fb3dae019004219058
Molly LeCompte <mollyklecompte@gmail.com>
2018-04-05 16:20:00 -0700

    fixed embeddings.py to leave out README from  data files

--- embeddings.py
+++ embeddings.py
@@ -12,7 +12,7 @@ from ast import literal_eval
 import tensorflow as tf
 
 
-data_files = [f'tweet_data/{f}' for f in os.listdir('tweet_data/')]
+data_files = [f'tweet_data/{f}' for f in os.listdir('tweet_data/') if f != 'README']
 
 # returns list of first 1500 lines of tweet text csv from a file
 def get_tweet_text(file):
@@ -20,7 +20,7 @@ def get_tweet_text(file):
     with open(file, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            if len(tweets) <= 1500:
+            if len(tweets) <= 1499:
                 tweets.append(row[0])
             else:
                 break
@@ -37,8 +37,17 @@ def char_ngrammer(input_str, n):
 def tweets_to_ngrams(files: list, n):
     ngrams = []
     for f in files:
-        tweet_texts = get_tweet_text(f)[:4]
-        # creates ngrams for text of DECODED tweet with links removed
-        tweet_grams = [(re.sub(r'https?:\/\/\S*[\r\n]*', '', literal_eval(t).decode('utf-8'), flags=re.MULTILINE)) for t in tweet_texts]
+        tweet_texts = get_tweet_text(f)
+        # creates ngrams for text of DECODED tweet
+        for t in tweet_texts:
+            try:
+                literal_eval(t).decode('utf-8')
+            except:
+                print("BAD STRING: ", t)
+        tweet_grams = [char_ngrammer(re.sub(r'https?:\/\/\S*[\r\n]*', '', literal_eval(t).decode('utf-8'), flags=re.MULTILINE), n) for t in tweet_texts]
         ngrams.extend(tweet_grams)
     return ngrams
+
+x = tweets_to_ngrams(data_files, 2)
+print(x[:20])
+print(len(x))
