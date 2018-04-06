ef9105120bb297a50254cf9eb462a74d22f4fbaa
Molly LeCompte <mollyklecompte@gmail.com>
2018-04-06 14:38:53 -0700

    add padding to shorter inputs so all are len 280

--- embeddings.py
+++ embeddings.py
@@ -7,12 +7,17 @@ import collections
 import csv
 import os
 import re
+import gensim
+import numpy as np
 from ast import literal_eval
 
 import tensorflow as tf
 
 
 data_files = [f'tweet_data/{f}' for f in os.listdir('tweet_data/') if f != 'README']
+dim_embed = 300
+input_len = 280
+window = 5
 
 # returns list of first 1500 lines of tweet text csv from a file
 def get_tweet_text(file):
@@ -30,7 +35,8 @@ def get_tweet_text(file):
 
 # creates character n-grams from string (for single tweet)
 def char_ngrammer(input_str, n):
-    return [input_str[ind:ind+n] for ind in range(len(input_str)-n +1)]
+    padded_str = input_str+ '\0' * (input_len - len(input_str))
+    return [padded_str[ind:ind+n] for ind in range(len(padded_str)-n +1)]
 
 
 # returns list of ngram lists from all tweet text
@@ -39,15 +45,31 @@ def tweets_to_ngrams(files: list, n):
     for f in files:
         tweet_texts = get_tweet_text(f)
         # creates ngrams for text of DECODED tweet
-        for t in tweet_texts:
-            try:
-                literal_eval(t).decode('utf-8')
-            except:
-                print("BAD STRING: ", t)
-        tweet_grams = [char_ngrammer(re.sub(r'https?:\/\/\S*[\r\n]*', '', literal_eval(t).decode('utf-8'), flags=re.MULTILINE), n) for t in tweet_texts]
+        tweet_grams = [char_ngrammer(re.sub(r'https?:\/\/\S*[\r\n]*', '', literal_eval(t).decode('utf-8'), flags=re.MULTILINE).replace('&amp', '&'), n) for t in tweet_texts]
         ngrams.extend(tweet_grams)
     return ngrams
 
-x = tweets_to_ngrams(data_files, 2)
-print(x[:20])
-print(len(x))
+def create_embeddings(ngrams):
+    model = gensim.models.Word2Vec(ngrams, size=dim_embed, window=window, min_count=0, iter=10)
+
+    return model
+
+
+# model = create_embeddings(tweets_to_ngrams(data_files, 2))
+# model.save('models/embeddings')
+# print(len(model.wv.vocab))
+# print(list(model.wv.vocab.keys()))
+
+model = gensim.models.Word2Vec.load('models/embeddings')
+embedding_matrix = np.zeros((len(model.wv.vocab), dim_embed))
+for i in range(len(model.wv.vocab)):
+    embedding_vector = model.wv[model.wv.index2word[i]]
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+
+# print(len(model.wv.vocab))
+# print(model)
+# vocab_len = model.wv.vocab
+# initializer = np.zeros((vocab_len, dim_embed), dtype=np.float32)
+
